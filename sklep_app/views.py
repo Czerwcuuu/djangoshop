216abfb350bd3a1f1b0f9e0495949fe9a88cc52f
Czerwcuu <czerwiecr1999@gmail.com>
2020-05-19 14:26:48 +0200

    html template added, views

--- sklep_app/views.py
+++ sklep_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("Moja strona w django.")
+def main(request):
+    return render(request, 'sklep_app/index.html',{})
