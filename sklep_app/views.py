dcc405decd5d6e50f773f6496d27e3750558355a
Czerwcuu <czerwiecr1999@gmail.com>
2020-05-19 15:40:41 +0200

    pierwszy testowy szablon

--- sklep_app/views.py
+++ sklep_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Przedmiot
 
 def main(request):
-    return render(request, 'sklep_app/index.html',{})
+    przedmioty = Przedmiot.objects.all()
+    return render(request, 'sklep_app/index.html',{'przedmioty':przedmioty})
